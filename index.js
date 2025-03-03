d92abde121ed56800da1d8d28892cf98171ed137
yanasoup <yana.soup@gmail.com>
2025-03-03 15:49:57 +0700

    add vercel config

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const midtransRoutes = require('./routes/midtrans');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Vercel!' });
